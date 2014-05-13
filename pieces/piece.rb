e6deceacfc1bd3969bfb4db58cfa66c0c5e18580
= <=>
2014-05-13 10:16:16 -0400

    Add color to piece initializer

--- pieces/piece.rb
+++ pieces/piece.rb
@@ -1,11 +1,9 @@
 class Piece
   
-  def initialize(board, position)
+  def initialize(board, position, color)
     @board = board
     @position = position
+    @color = color
   end
-  
-  def moves
-    
-  end
+
 end
